3f9f9f5a4cc0480b76c1398c555ea24042017b69
Stefan J <sjohansson88@hotmail.com>
2023-11-28 15:39:26 +0100

    Fixed customer menu

--- CustomerMeny.cs
+++ CustomerMeny.cs
@@ -11,52 +11,39 @@ namespace DotNetDynamos
         public int GetMenyChoice()
         {
             Console.WriteLine("Customer Meny");
-            string menu1 = "1. View account and balance";
-            string menu2 = "2. Transfer money between accounts";
-            string menu3 = "3. Transfer money to other Customer";
-            string menu4 = "4. Open new account";
-            string menu5 = "5. Another currency";
-            string menu6 = "6. Account history";
-            string menu7 = "7. Logg out";
-            Console.WriteLine("\n\t" + menu1 + "\n\t" + menu2 + "\n\t" + menu3 + "\n\t" + menu4 + "\n\t" + menu5 + "\n\t" + menu6 + "\n\t" + menu7);
+            Console.WriteLine("View account and balance");
+            Console.WriteLine("2. Transfer money between accounts");
+            Console.WriteLine("3. Transfer money to other Customer");
+            Console.WriteLine("4. Open new account");
+            Console.WriteLine("5. Another currency");
+            Console.WriteLine("6. Account history");
+            Console.WriteLine("7. Logg out"); 
+            
             Console.Write("Choose meny: ");
-            if (!int.TryParse(Console.ReadLine(), out int choice))
-            {
-                Console.WriteLine("Invalid number");
-                return -1;
+            int choice = Convert.ToInt32(Console.ReadLine());
+      
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                         break;
+                    case 7:
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong input, try again.");
+                        break;
+                }
             }
-            return choice;
-        }
-        public void Meny()
-        {
-            int choice = GetMenyChoice();
-            switch (choice)
-            {
-                case 1:
-                    ShowBalance();
-                    break;
-                case 2:
-                    Transfer();
-                    break;
-                case 3:
-                    TransferToOthers();
-                    break;
-                case 4:
-                    NewAccount();
-                    break;
-                case 5:
-                    Currency();
-                    break;
-                case 6:
-                    AccountHistory();
-                    break;
-                case 7:
-                    LogOut();
-                    break;
-                defualt:
-                    break;
-            }
-        }
 
         static void ShowBalance()
         {
