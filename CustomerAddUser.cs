ca84b69cdf242ffb1fd09b7c83dd7faff98455e2
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-11-29 14:31:05 +0100

    Börjat dela upp customer

---
+++ CustomerAddUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetDynamos
+{
+    internal partial class Customer : AllUsers
+    {
+        public void AddUser(string username, string password, int IDnumber)
+        {
+            _username = username;
+            Password = password;
+            _IDnumber = IDnumber;
+            CustomerUsers.Add(IDnumber, password);
+        }
+    }
+}
