7a126a4d0ab1b064da5fef0387ed64e0229f959b
Philip Jönsson <fillejoensson@hotmail.com>
2023-11-29 14:57:08 +0100

    Added partial class

--- Admin.cs
+++ Admin.cs
@@ -12,25 +12,5 @@ namespace DotNetDynamos
         public static Dictionary<int, string> AdminUsers = new Dictionary<int, string>();
         private static int nextAdID = 1001;
         private static int maxLoginAttempts = 3;
-
-
-        public static void AddUser(int userId, string password)
-        {
-            AdminUsers.Add(userId, password);
-        
-        }
-
-       
-
-        public override void UserList()
-        {
-            Console.WriteLine("Customer Users:");
-            foreach (KeyValuePair<int, string> adminUser in AdminUsers)
-            {
-                Console.WriteLine($"ID: {adminUser.Key}, Username: {adminUser.Value}");
-            }
-        }
-
-
     }
 }
---
+++ AdminAddUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetDynamos
+{
+    internal partial class Admin : AllUsers
+    {
+        public static void AddUser(int userId, string password)
+        {
+            AdminUsers.Add(userId, password);
+
+        }
+    }
+}
