e2571ccf6ddf8a0c53bb11cbbb423272bcac4e92
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-11-29 14:38:36 +0100

    Delat upp Customer helt

---
+++ CustomerTransfer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetDynamos
+{
+    internal partial class Customer : AllUsers
+    {
+        static void Transfer()
+        {
+
+
+        }
+        static void TransferToOthers()
+        {
+
+        }
+    }
+}
