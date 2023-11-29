caeaae879cf3dba043bff4171e37cb6890274155
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-11-29 15:09:35 +0100

    -

--- AdminInfo.cs
+++ AdminInfo.cs
@@ -11,6 +11,7 @@ namespace DotNetDynamos
     {
         public static Dictionary<int, string> AdminUsers = new Dictionary<int, string>();
         private static int nextAdID = 1001;
+        private static int maxLoginAttempts = 3;
 
     }
 }
