50dad1fe5978bfe26ca73bfa7c1ecdd0b6958ab6
askahana <hanadasuka@gmail.com>
2023-11-30 12:56:28 +0100

    ShowBalance() in Customerclass

--- CustomerShowBalance.cs
+++ CustomerShowBalance.cs
@@ -8,9 +8,26 @@ namespace DotNetDynamos
 {
     internal partial class Customer : AllUsers
     {
-        static void ShowBalance()
+        // This is to show the balance on all accounts.
+        static void ShowBalance(Dictionary<int, List<Account>> userAccounts, int id)
         {
-
+            if (userAccounts.ContainsKey(id))
+            {
+                if (userAccounts[id].Count <= 0) // or maybe "==0"
+                {
+                    Console.WriteLine("You do not have yet any accounts.");
+                    return;
+                }
+                Console.WriteLine("Here are your accounts");
+                foreach (var account in userAccounts[id])  
+                {
+                    Console.WriteLine($"Account ID: {account.AccountNumber}\nBalance: {account.Balance}");   
+                }
+            }
+            else // Maybe we do not need else statement becuase you cannot log in without id.
+            {
+                Console.WriteLine("User not found.");
+            }
         }
     }
 }
