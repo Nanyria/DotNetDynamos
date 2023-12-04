fd7718dcdce0c918b04f045587d59d9bf4b748f4
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-12-05 00:21:09 +0100

    Påbörjat fixa ShowBalance, behöver jobbas mer med.

--- CustomerShowBalance.cs
+++ CustomerShowBalance.cs
@@ -9,25 +9,17 @@ namespace DotNetDynamos
     internal partial class Customer : AllUsers
     {
         // This is to show the balance on all accounts.
-        static void ShowBalance(Dictionary<int, List<Account>> userAccounts, int id)
+        static void ShowBalance(AllUsers loggedInCustomer)
+
         {
-            if (userAccounts.ContainsKey(id))       // maybe we do not need this if-statement?
+
+            Console.WriteLine("Here are your accounts");
+            foreach () //Acc in loggedInCustomer
             {
-                if (userAccounts[id].Count <= 0) 
-                {
-                    Console.WriteLine("You do not have yet any accounts.");
-                    return;
-                }
-                Console.WriteLine("Here are your accounts");
-                foreach (var account in userAccounts[id])  
-                {
-                    Console.WriteLine($"Account ID: {account.AccountNumber}\nBalance: {account.Balance}");   
-                }
+                Console.WriteLine($"Account ID: {account.AccountNumber}\nBalance: {account.Balance}");
             }
-            else // Maybe we do not need else-statment because you cannot come here without id.
-            {
-                Console.WriteLine("User not found.");
-            }
+
+
         }
     }
 }
